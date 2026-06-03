ea9e789f8ae61aabf20d5959252895fc976b1e2a
Digvijay Kesare <digvijaykesare1123@gmail.com>
2026-06-03 19:41:52 +0530

    fixed totalHead casing

--- app/api/createSurvey/route.ts
+++ app/api/createSurvey/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
           discharge: body.discharge,
           staticheight: body.staticHeight,
           rows: body.rows,
-          totalhead: body.totalHead,
+          totalHead: body.totalHead,
           userid: decoded.userId
         }
       ])
